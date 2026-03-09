f470cead2a1226c7b49bdd0b83a7f68be72ca213
Visvesvararao <viswv011@gmail.com>
2026-03-10 00:07:21 +0530

    changed css

--- src/MainImage.jsx
+++ src/MainImage.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const MainImage = () => {
   return (
-    <div id='MainImage'>
-      <h3 id='heading'>Our image will come here</h3>
+    <div style={{width:'100vw'}}>
+      <div id='MainImage'>
+        <h3 id='heading'>Our image will come here</h3>
+      </div>
+    <h3 style={{
+  textAlign: "center",
+  fontSize: "2vh",
+  background: "linear-gradient(90deg,#ff4fa3,#ff9bd2)",
+  WebkitBackgroundClip: "text",
+  color: "#ff4fa3",
+  textShadow: "0px 0px 0.1vh black,0px 0px 0.1vh black,0px 0px 0.1vh black,0px 0px 0.1vh black,0px 0px 0.1vh black,0px 0px 0.1vh black,0px 0px 0.1vh black,0px 0px 0.1vh black,0px 0px 0.1vh black,0px 0px 0.1vh black,0px 0px 0.1vh black,0px 0px 0.1vh black"
+}}>
+  Made with ❤️ by spidy
+</h3>
     </div>
   )
 }
